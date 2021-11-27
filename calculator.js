1f3d69c5740fef0d25ba9159effc5bd8cb0df63a
Chris Castle <chrisjcastle93@gmail.com>
2021-11-27 14:23:58 +0100

    done

--- calculator.js
+++ calculator.js
@@ -1,17 +1,38 @@
 function sum(a, b) {
-  return;
-}
+  if (a == null && b == null) {
+    return 0;
+  } else if (a == null || b == null) {
+      if(a == null) {
+        return 0 + b;
+      } else if (b == null) {
+        return 0 + a;
+      }
+  } else {
+    return a + b;
+  }}
 
 function subtract(a, b) {
-  return;
-}
+  if (a == null && b == null) {
+    return 0;
+  } else if (a == null || b == null) {
+      if(a == null) {
+        return b - 0;
+      } else if (b == null) {
+        return a - 0;
+      }
+  } else {
+    return a - b;
+  }}
 
 function divide(a, b) {
-  return;
+  if (a == 0 || b == 0) {
+    throw new Error('An explanatory error message')  } else {
+    return a / b;
+  }
 }
 
 function multiply(a, b) {
-  return a * b;
+  return;
 }
 
 // The following is required to make unit tests work. Please ignore it.
